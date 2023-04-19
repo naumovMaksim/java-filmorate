28102f1dbc5520e08d968738a166c9dc739578ab
Наумов Максим <dusa11223344@yandex.ru>
2023-04-19 10:59:58 +0300

    Style: исправления

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
@@ -6,8 +6,12 @@ import java.util.Collection;
 
 public interface FilmStorage {
     Film findFilm(int id);
+
     Collection<Film> findAll();
+
     Film create(Film film);
+
     Film update(Film film);
+
     Collection<Film> getPopularFilms(Integer count);
 }
